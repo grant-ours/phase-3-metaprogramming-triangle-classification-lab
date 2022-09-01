8f057138d4f925ba17d7306ca617a64fd1b492f0
Grant Ours <grant.ours55@gmail.com>
2022-09-01 12:56:20 -0600

    commit tree 68f42aead0903a5ca3f04893cabb4d6ebfe1b04f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+  def kind
+    if(@s1 > 0 && @s2 > 0 && @s3 > 0)
+      if((@s1+@s2)>@s3 && (@s1+@s3)>@s2 && (@s2+@s3)>@s1)
+        if(@s1 == @s2 && @s1 == @s3)
+          :equilateral
+        elsif(@s1 == @s2 || @s1 == @s3 || @s2 == @s3)
+          :isosceles
+        else
+          :scalene
+        end
+      else
+        raise TriangleError
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
